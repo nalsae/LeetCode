30e5b33512e1aa09ccc71cdaab394cb3c85c3910
nalsae <101828759+nalsae@users.noreply.github.com>
2022-06-06 23:16:42 +0900

    Time: 65 ms, Memory: 42.9 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    if(!rowIndex) return [1];
+    
+    let res = [1, 1]
+    
+    while(--rowIndex) {
+        const next = [];
+        
+        for(let i = 0; i < res.length-1; i++) {
+            next.push(res[i] + res[i+1]);
+        }
+        res = [1, ...next, 1];
+    }
+    return res;
+};

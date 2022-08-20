50952823abec5ea61bfd76120fd9e892374539bf
nalsae <101828759+nalsae@users.noreply.github.com>
2022-08-20 23:09:42 +0900

    Time: 100 ms (56.56%), Space: 43.5 MB (93.75%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    let answer = [];
+    
+    for (let i = 0; i < prices.length; i++){
+        let difference = 0;
+        
+        for (let j = i + 1; j < prices.length; j++){
+            if (prices[j] <= prices[i]){
+            difference = prices[j];
+                
+            break;
+            }
+        }
+        answer.push(prices[i] - difference);
+    }
+    
+    return answer;
+};

f6a1a7931429010ddd0305826d813a131fed7f94
nalsae <101828759+nalsae@users.noreply.github.com>
2022-08-27 20:29:44 +0900

    Time: 75 ms (84.91%), Space: 44.2 MB (26.04%) - LeetHub

---
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {number}
+ */
+const countPrefixes = function(words, s) {
+    let count = 0;
+    
+    for(i = 0; i < words.length; i++){
+        for(j = 1; j <= s.length; j++){
+            if(words[i] == s.slice(0, j)){
+                count++;
+            }
+        }        
+    }
+    
+    return count;
+};

57a372436ead9f9c7a9da783a4bc83e1e51873b8
nalsae <101828759+nalsae@users.noreply.github.com>
2022-10-15 18:18:03 +0900

    Time: 73 ms (90.31%), Space: 44.3 MB (50.88%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+const destCity = (paths) => {
+  const set = new Set();
+    
+  for (const path of paths) set.add(path[0]);
+    
+  for (const path of paths) {
+    if (!set.has(path[1])) return path[1];
+  }
+};

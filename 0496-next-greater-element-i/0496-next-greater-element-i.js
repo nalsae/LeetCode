b344f38034d5a82633afc4473db749a48f61a5b0
nalsae <101828759+nalsae@users.noreply.github.com>
2022-11-04 21:32:26 +0900

    Time: 103 ms (66.80%), Space: 44.1 MB (50.62%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const nextGreaterElement = (nums1, nums2) => {
+  const map = {};
+  const stack = [];
+    
+  nums2.forEach(n => {
+    while (stack.length > 0 && stack[stack.length - 1] < n) {
+      map[stack.pop()] = n;
+    }
+    stack.push(n);
+  });
+    
+  return nums1.map(n => map[n] || -1);
+};

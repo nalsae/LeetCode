167f7bfb69584c1fdd4b29e8c1fe5e8820640d98
nalsae <101828759+nalsae@users.noreply.github.com>
2022-11-08 17:33:35 +0900

    Time: 100 ms (48.98%), Space: 41.9 MB (97.01%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const uniqueOccurrences = (arr) => {
+    const obj = {};
+    
+    arr.forEach(val => obj[val] = (obj[val] || 0) + 1);
+    
+    const val = Object.values(obj).sort((a, b) => a - b);
+    
+    for (let i = 0; i < val.length - 1; i++){
+        if (val[i] === val[i + 1]) return false;
+    }
+    
+    return true;
+};

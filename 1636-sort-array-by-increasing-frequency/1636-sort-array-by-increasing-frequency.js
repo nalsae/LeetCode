7f66d562ba7bb0cb7019501d5b2adfabeaf575e3
nalsae <101828759+nalsae@users.noreply.github.com>
2022-12-02 21:24:50 +0900

    Time: 115 ms (72.26%), Space: 44.4 MB (74.09%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const frequencySort = (nums) => {
+    const map = new Map();
+    
+    for (let n of nums) {
+        map.set(n, (map.get(n) + 1) || 1);
+    }
+    
+    return nums.sort((a, b) => map.get(a) - map.get(b) || b - a);
+};
